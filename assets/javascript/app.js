eb301d1c3d1a4d18337d97475a5f878e6909a26e
Caroline Bowes <carvbowes@gmail.com>
2017-07-03 19:01:23 -0400

    added individual entry deletion, changed input error alert

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,8 +13,18 @@ $(document).ready(function () {
 
   var database = firebase.database().ref();
 
-  function addRow(snapshotObj) {
-  	var newRow = $("<tr>");
+  function convertToMinutes(time) {
+ 	if ($("#units-select").val() === "hours") {
+  		return time * 60;
+
+  	} else {
+  		return time;
+  	}
+  }
+
+  //adds new row to table of train info
+  function addRow(snapshotObj, key) {
+  	var newRow = $("<tr>").attr("id", key);
 
   	var minsAway = calculateMinsAway(snapshotObj.start, snapshotObj.frequency);
   	console.log(minsAway);
@@ -25,6 +35,7 @@ $(document).ready(function () {
   	newRow.append("<td>" + snapshotObj.frequency + "</td>");
   	newRow.append("<td>" + nextTrainTime(minsAway) + "</td>");
   	newRow.append("<td>" + minsAway + "</td>");
+  	newRow.append("<td> <span class='delete'> x </span></td>");
 
   	$("#schedule-body").append(newRow);
 
@@ -50,23 +61,40 @@ $(document).ready(function () {
   	return moment().add(mins, "minutes").format("hh:mm");
   }
 
+  //alert user of input error
+  function errorAnimate() {
+  	$("#train-form").css("background-color", "#bc7974");
+  	$("#train-form").animate({backgroundColor: "rgb( 255, 255, 255)"});
+  }
+
+  $(document).on("click", ".delete", function () {
+ 	var parent = $(this).parents("tr");
+  	firebase.database().ref(parent.attr("id")).remove(); //removes node with given ID from firebase
+	
+  });
+
   $("#add-train").on("click", function(event) {
   	event.preventDefault();
 	var nameInput = $("#name-input").val().trim();
   	var destinationInput = $("#destination-input").val().trim();
   	var startInput = $("#start-time-input").val().trim();
-  	var frequencyInput = $("#frequency-input").val().trim();
+  	var frequencyInput = convertToMinutes(parseInt($("#frequency-input").val().trim()));
 
   	//regex to check time input
   	var p = /([01]?[0-9]|2[0-3]):[0-5][0-9]/;
 
   	if (nameInput === "" || destinationInput === "" || startInput === "" || frequencyInput === "") {
-  		alert("Please enter all train info");
+  		errorAnimate();
+  		$("#error").show();
   		
   	} else if (!p.test(startInput)) {
-  		alert("please enter a valid time");
+  		errorAnimate();
+  		$("#time-error").show();
 
   	} else {
+  		$("#error").hide();
+  		$("#time-error").hide();
+
   		database.push({
 	  		name: nameInput,
 	  		destination: destinationInput,
@@ -86,8 +114,17 @@ $(document).ready(function () {
 
   database.on("child_added", function(snap) {
   	var value = snap.val();
+  	var key = snap.key;	//key will be used as row ID for row deletion
 
-  	addRow(value);
+  	addRow(value, key);
+
+  }, function(err) {
+  	console.log("Errors handled: " + err.code);
+  });
+
+  //row deletion
+  database.on("child_removed", function(snap) {
+  	$("#" + snap.key).remove();
 
   }, function(err) {
   	console.log("Errors handled: " + err.code);
